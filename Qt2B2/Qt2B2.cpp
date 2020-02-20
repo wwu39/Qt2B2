58cbe1dc3d3bdbfc8eefd095cb682c1a6058f9c2
Weikai Wu <wwu39@ucsc.edu>
2020-02-20 04:02:50 -0800

    7y868

--- Qt2B2/Qt2B2.cpp
+++ Qt2B2/Qt2B2.cpp
@@ -6,21 +6,22 @@
 #include <filesystem>
 #include <string>
 #include <direct.h>
+#include <functional>
 
 // Commands
-//#define BGM
-//#define BGS
-//#define FADEBGM
-//#define FADESE
-//#define BG
-//#define SE
-//#define UNLOCKMUSIC
-//#define UNLOCKCG
-//#define BRANCH
-//#define IMAGE
-//#define GAME
-//#define AUTOSAVE
-//#define WAIT
+#define BGM
+#define BGS
+#define FADEBGM
+#define FADESE
+#define BG
+#define SE
+#define UNLOCKMUSIC
+#define UNLOCKCG
+#define BRANCH
+#define IMAGE
+#define GAME
+#define AUTOSAVE
+#define WAIT
 #define CAMERA
 
 namespace fs = std::filesystem;
@@ -32,6 +33,20 @@ static ofstream logfile;
 
 string path = IsDebug ? "C:\\Users\\24233\\Desktop\\286studio\\Ballade1Qt-master\\Ballade1\\script\\chs" : ".";
 string output_path = path + "\\OUTPUT";
+
+hash<string> hasher;
+
+bool isSqrPrent(char c) { return c == '[' || c == ']'; }
+
+string ftos(float f)
+{
+    string ret = to_string(f);
+    ret.erase(ret.find_last_not_of('0') + 1, string::npos);
+    if (ret[ret.size() - 1] == '.') {
+        ret.erase(ret.end() - 1);
+    }
+    return ret;
+}
 
 string GetFilenameFromPath(string path)
 {
@@ -123,6 +138,7 @@ void processSingleFile(string path)
             auto semicolonIdx = line.find_first_of(';');
             if (commandStartIdx != string::npos && semicolonIdx < commandStartIdx) continue;
             if (commandStartIdx2 != string::npos && semicolonIdx < commandStartIdx2) continue;
+            int firstNotSpace = line.find_first_not_of(' ');
 #ifdef BGM
             if (commandStartIdx != string::npos && line.substr(commandStartIdx, 4) == "@bgm")
             {
@@ -307,34 +323,54 @@ void processSingleFile(string path)
             if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 13) == "+image.group(") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
-                line2 = SetParams("@image()", { GetParams(line)[0], "\'\'" });
+                int z = stoi(GetParams(line)[0]);
+                z += 10;
+                //line2 = SetParams("@image()", { to_string(z), "\'none.png\'" });
+                line2 = ";" +line;
             }
             if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 13) == "+image.image(") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
                 auto params = GetParams(line);
-                line2 = SetParams("@image()", params);
-                string nl = SetParams("\n@setScale()", { params[0], line.substr(line.find_last_of('=') + 1) });
-                line2 += nl;
+                int z = stoi(params[0]);
+                z += 10;
+                string newParam = "{type=\'image\',name=\'z" + to_string(z) + "\',path=" + params[1] + ",anchor=\'0.23,0.35\',z=" + to_string(z) + ",scale=" + line.substr(line.find_last_of('=') + 1) +'}';
+                line2 = SetParams("@img.new()", { newParam });
             }
             if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 11) == "+image.add(") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
                 string temp = line.substr(commandStartIdx2 + 22);
-                string z1 = temp.substr(0, temp.find_first_of(')'));
+                int z1 = stoi(temp.substr(0, temp.find_first_of(')')));
                 temp = temp.substr(temp.find_first_of('('));
-                string z2 = temp.substr(1, temp.find_first_of(')') - 1);
-                line2 = SetParams("@setChild()", { z1,z2 });
+                int z2 = stoi(temp.substr(1, temp.find_first_of(')') - 1));
+                //z1 += 10;
+                z1 = 0;
+                z2 += 10;
+                //line2 = SetParams("@setChild()", { to_string(z1), to_string(z2) });
+                line2 = ";" + line;
             }
             if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 14) == "+image.setPos(") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
-                line2 = SetParams("@setPosition()", GetParams(line));
+                auto params = GetParams(line);
+                int z = stoi(params[0]);
+                z += 10;
+                float x = stof(params[1]);
+                //x += 0.5;
+                x *= 0.85;
+                float y = stof(params[2]);
+                //y += 0.5;
+                y = -y;
+                line2 = SetParams("@setPosition()", { to_string(z), ftos(x), ftos(y) });
             }
             if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 18) == "+image.setOpacity(") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
-                line2 = SetParams("@setAlpha()", GetParams(line));
+                auto params = GetParams(line);
+                int z = stoi(params[0]);
+                z += 10;
+                line2 = SetParams("@setAlpha()", {to_string(z), params[1]});
             }
 #endif
 
@@ -342,12 +378,27 @@ void processSingleFile(string path)
             if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 15) == "+game.showPort=") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
-                line2 = "+game._showPort" + line.substr(line.find_last_of('='));
+                line2 = "@game._showPort" + line.substr(line.find_last_of('='));
             }
-            if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 27) == "+game.r=Notification.result") {
+            else if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 17) == "+game._skipRatio=") {
+                logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
+                nochange = false;
+                line2 = "@game._skipRatio" + line.substr(line.find_last_of('='));
+            }
+            else if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 9) == "+game.cc=") {
+                logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
+                nochange = false;
+                line2 = "@game.cc" + line.substr(line.find_last_of('='));
+            }
+            else if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 27) == "+game.r=Notification.result") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
                 line2 = ";" + line;
+            }
+            else if (commandStartIdx2 != string::npos && line.substr(commandStartIdx2, 6) == "+game.") {
+                logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
+                nochange = false;
+                line2 = "@game" + line.substr(line.find_first_of('.'));
             }
 #endif
 
@@ -360,7 +411,6 @@ void processSingleFile(string path)
 #endif
 
 #ifdef WAIT
-            int firstNotSpace = line.find_first_not_of(' ');
             if (firstNotSpace < line.length() && line[firstNotSpace] == '=') {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
                 nochange = false;
@@ -376,6 +426,50 @@ void processSingleFile(string path)
 #ifdef CAMERA
             if (commandStartIdx != string::npos && line.substr(commandStartIdx, 8) == "@camera(") {
                 logfile << LineNumToString(linum) << " CHECKING: " << line << endl;
+                nochange = false;
+                line.erase(remove_if(line.begin(), line.end(), isSqrPrent), line.end());
+                auto params = GetParams(line);
+                string newParam;
+                int z = stoi(params[0]);
+                if (z > 4) z += 10;
+                float time = stoi(params[3]);
+                time /= 1000;
+                char easing;
+                if (params[4] == "Easing.Linear") 
+                {
+                    easing = '=';
+                }
+                else if (params[4] == "Easing.OutSine")
+                {
+                    easing = '>';
+                }
+                if (params[2] == "\'x\'")
+                {
+                    string endX = params[5].substr(0, params[5].find_first_of('*'));
+                    float x = stof(endX);
+                    //x += 0.5;
+                    x *= 0.85;
+                    newParam = "\'m" + ftos(time) + easing + ftos(x) + ",_\'";
+                }
+                else if (params[2] == "\'y\'")
+                {
+                    string endY = params[5].substr(0, params[5].find_first_of('*'));
+                    float y = stof(endY);
+                    //y += 0.5;
+                    y = -y;
+                    newParam = "\'m" + ftos(time) + easing + "_," + ftos(y) + '\'';
+                }
+                else if (params[2] == "\'opacity\'")
+                {
+                    string X = "";
+                    if (params[5] == "0") X = " x";
+                    newParam = "\'a" + ftos(time) + easing + params[5] + X + '\'';
+                }
+                line2 = (firstNotSpace == 0 ? "" : line.substr(0, firstNotSpace)) + SetParams("@camera()", {
+                    (z == 4 ? "ManualFixNeeded" : to_string(z)),
+                    to_string(hasher(params[1])),
+                    newParam
+                    });
             }
 #endif
 
@@ -395,7 +489,6 @@ void processSingleFile(string path)
 
 int main()
 {
-    cout << *desktop_directory() << endl;
     if (_mkdir(output_path.c_str()))
         cout << "Create folder " + output_path << endl;
     else
